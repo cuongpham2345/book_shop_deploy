eac04ea2ce04fbb6e67ed1dcd03e766586a38b5e
shoppe <cuonzpham@gmail.com>
2026-05-16 15:31:55 +0700

    refactor: read RSA keys from env variables

--- backend/src/main/java/buy_book/properties/RsaKeyConfigProperties.java
+++ backend/src/main/java/buy_book/properties/RsaKeyConfigProperties.java
@@ -1,19 +1,48 @@
 package buy_book.properties;
 
-import lombok.Data;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.security.KeyFactory;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
 
 @Component
-@Data
-@ConfigurationProperties(prefix = "rsa")
+@Getter
 public class RsaKeyConfigProperties {
-    RSAPrivateKey privateKey;
 
-    RSAPublicKey publicKey;
+    private final RSAPublicKey publicKey;
+    private final RSAPrivateKey privateKey;
 
+    public RsaKeyConfigProperties(
+            @Value("${RSA_PUBLIC_KEY}") String publicKeyPem,
+            @Value("${RSA_PRIVATE_KEY}") String privateKeyPem
+    ) throws Exception {
+        this.publicKey = parsePublicKey(publicKeyPem);
+        this.privateKey = parsePrivateKey(privateKeyPem);
+    }
 
+    private RSAPublicKey parsePublicKey(String pem) throws Exception {
+        String clean = pem
+                .replace("-----BEGIN PUBLIC KEY-----", "")
+                .replace("-----END PUBLIC KEY-----", "")
+                .replaceAll("\\s+", "");
+        byte[] decoded = Base64.getDecoder().decode(clean);
+        return (RSAPublicKey) KeyFactory.getInstance("RSA")
+                .generatePublic(new X509EncodedKeySpec(decoded));
+    }
+
+    private RSAPrivateKey parsePrivateKey(String pem) throws Exception {
+        String clean = pem
+                .replace("-----BEGIN PRIVATE KEY-----", "")
+                .replace("-----END PRIVATE KEY-----", "")
+                .replaceAll("\\s+", "");
+        byte[] decoded = Base64.getDecoder().decode(clean);
+        return (RSAPrivateKey) KeyFactory.getInstance("RSA")
+                .generatePrivate(new PKCS8EncodedKeySpec(decoded));
+    }
 }
